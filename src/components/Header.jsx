f35348afed3da1b438607977583dc1033cd999a4
pinkythawani <thawanipinky36@gmail.com>
2026-02-02 23:08:23 +0530

    basic navigation

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => 
+{
+  return (
+    <h1>Welcome to Header Component</h1>
+  )
+}
+
+export default Header;
---
+++ src/components/SingleProduct.jsx
@@ -0,0 +1,7 @@
+const SingleProduct = () => {
+    return (
+        <h1>Welcome to Single Product</h1>
+    )
+
+}
+export default SingleProduct;
