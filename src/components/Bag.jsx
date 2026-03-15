c7d238ded840eb25e9783486dcc786f45c3fa60b
pinkythawani <thawanipinky36@gmail.com>
2026-03-15 21:16:36 +0530

    css updated for product and productlist

--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -1,21 +1,31 @@
-const Bag = ({cartItems,removeFromCart}) => {
-    return(
-        <div>
-            <h2>Your Bag</h2>
-            {cartItems.length ===0 ? (
-                <p>Your bag is empty</p>
-            ):(
-                cartItems.map((p) => (
-                    <div key={p.id}>
-                         <img src={p.image} width="100" />
-                         <h4>{p.title}</h4>
-                         <p>{p.price}</p>
-                         <button onClick={() => removeFromCart(p.id)}>Remove</button>
-                    </div>
-                ))
-            )}
+const Bag = ({ cartItems, removeFromCart }) => {
+  return (
+    <div className="bag-container">
+      <h2>Your Bag</h2>
+
+      {cartItems.length === 0 ? (
+        <p className="empty">Your bag is empty</p>
+      ) : (
+        <div className="bag-items">
+          {cartItems.map((p) => (
+            <div className="bag-item" key={p.id}>
+              <img src={p.image} alt={p.title} />
+
+              <div className="bag-info">
+                <h4>{p.title}</h4>
+                <p className="price">Rs.{p.price}</p>
+                <button onClick={() => removeFromCart(p.id)}>
+                  Remove
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
+
+
 
 export default Bag;
